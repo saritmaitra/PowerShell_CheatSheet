00fdec20e693479ef0739a81dc291d8dd39023c6
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-06 22:12:33 +0800

    Create fundamentals.ps1

---
+++ fundamentals.ps1
@@ -0,0 +1,10 @@
+# powershell version
+ Get-Host | Select-Object Version
+ 
+ # version details in table
+ $psversiontable
+ 
+ # Windows PowerShell Integrated Scripting Environment (ISE)
+ # https://docs.microsoft.com/en-us/powershell/scripting/windows-powershell/ise/introducing-the-windows-powershell-ise?view=powershell-7.1
+ powershell_ise.exe
+ 
