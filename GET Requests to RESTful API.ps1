f6bb13b6b770e68182bdefc65776eae34666dc18
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-12 11:27:25 +0800

    Update GET Requests to RESTful API.ps1

--- GET Requests to RESTful API.ps1
+++ GET Requests to RESTful API.ps1
@@ -1,5 +1,26 @@
 # Invoke-RestMethod
 
-1. End-point
-2. 
+## 1. Endpoint (Location of API data which is 'RestMethod' here)
+## 2. Method ('Get' method - collect information from API; Method describes how we want to interact with an endpoint.
+# how they are used will vary across different APIs) 
+## 3. Content Type (Content in Modern RESTful APIs usually delivered in json format)
+
+# EXAMPLE: https://www.reddit.com/
+# https://www.reddit.com/.json
+
+$data = Invoke-RestMethod -Method Get -Uri 'https://www.reddit.com/.json
+
+$data # look into the variable
+
+$data | Get-Member # json data is taken as PowerShell object
+
+$data.data
+
+$data.data.children
+
+$data.data.children[0]
+
+$data.data.children[0].data
+
+$data.data.children[1].data
   
